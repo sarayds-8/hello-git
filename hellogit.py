7ccbb8c4f4ca9a4bf13594b1c4e9d850dd38d9cc
sarayds-8 <saray97.8.22@gmail.com>
2025-04-06 19:17:46 +0200

    Este es otro commit

--- hellogit.py
+++ hellogit.py
@@ -1,7 +1,6 @@
 '''
 Terminal
------------
-Comandos de inicio:
+----------- COMANDOS INICIALES -----------
 
 ls # Listar archivos
 cd Desktop # Cambiar de directorio
@@ -17,17 +16,19 @@ git status # Ver el estado de los archivos
 # aparece el fichero en el que estamos pero dice que no hay 'commits'. No tiene guardado el archivo de ninguna forma en git.
 # Ahora mismo estamos trabajando en local. No estamos trabajando en la nube.
 
------------
-Añadir un archivo a la zona de preparación (staging):
+----------- GUARDAR CAMBIOS EN GIT -----------
+
+1º Añadir un archivo a la zona de preparación (staging):
 git add hellogit.py # Añadir un archivo a la zona de preparación. Aun no tengo commits, pero ya tenemos un archivo.
------------
-Crear un commit (hacer una fotografia):
+
+2º Crear un commit (hacer una fotografia):
 git commit # Guardar el archivo en git. Se crea un 'commit' que es un punto de guardado.
 # Se abre un editor de texto para escribir un mensaje. Es obligatorio escribir un mensaje para identificar el commit.
 
 git commit -m "Mensaje" # Hacer un commit con un mensaje directamente
 # Cada commit tiene un identificador único. Es un código de 40 caracteres que identifica el commit.
 
+---------- ESTADO DE LOS ARCHIVOS -----------
 git log # Ver los commits que hemos hecho
 git log --graph # Ver los commits que hemos hecho de forma gráfica. Para salir pulsar 'q'.
 git log --graph --pretty=oneline # Ver los commits que hemos hecho en una sola linea
@@ -35,20 +36,34 @@ git log --graph --decorate --all --oneline # Ver los commits que hemos hecho en 
 
 git status # Ver el estado de los archivos
 
------------
-Volver a un commit anterior:
+git diff # Ver los cambios que hemos hecho en el archivo. Nos dice que cambios hemos hecho desde el último commit sin hacer un commit.
+
+----------- VOLVER A COMMIT ANTERIOR -----------
 git checkout hellogit2.py # Volver a un commit anterior (situarnos en un punto concreto). 
 ¡¡¡¡ Se pierden los cambios que hayamos hecho desde ese commit !!!!
 
 git reset # Volver a un commit anterior. Se pierden los cambios que hayamos hecho desde ese commit.
+git reset --hard ID_COMMIT # Hace un reset a un commit anterior. Se pierden los cambios que hayamos hecho desde ese commit. 
 
+Siempre que hayas commiteado un cambio eso esta guardado, aunque hayas hecho un reset hard.
 
---------
-Crear un alias para un comando (util para comandos largos):
+git reflog # Ver el historial completo de commits que hemos hecho, no solo hasta el último commit.
+git reflog --graph # Ver el historial completo de commits que hemos hecho de forma gráfica. Para salir pulsar 'q'.
+---------
+Ejempolo de reset:
+git reset --hard ID_COMMIT1 anterior # Se pieren los cambios que hayamos hecho desde ese commit. 
+git chechout ID_COMMIT2 que habiamos eliminado # el main va a estar en un lado y la head apunta a otro lado.
+git reset --hard ID_COMMIT2 # volvemos a un commit que habiamos eliminado usando el mismo reset
+
+-------- ALIAS --------
+Crear un alias para un comando (util para comandos largos)
 git config --global alias.tree "log --graph --decorate --all --oneline" # creamos un alias llamado 'tree' para el comando 
 
---------
+-------- GIT TAG --------
+git tag nombre_tag
 
+Los tag se suelen usar para señalar puntos importantes en el tiempo, como por ejemplo una version de un programa.
+Buena practica: todos en minuscula y sin espacios. Se pueden usar guiones bajos o guiones medios.
 
 
 '''
